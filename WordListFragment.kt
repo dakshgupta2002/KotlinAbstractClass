ab75d8b5304e44fb118b511148196960b3e44b00
Daksh Gupta <78641951+dakshgupta2002@users.noreply.github.com>
2021-05-06 19:50:04 +0530

    Add files via upload
    Using fragments to navigate between two screens under one activity.
    
    NavGraph, NavHost and NavController all combined produced this amazing effect.

---
+++ WordListFragment.kt
@@ -0,0 +1,42 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+class WordListFragment : Fragment() {
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+    private var _binding : FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
